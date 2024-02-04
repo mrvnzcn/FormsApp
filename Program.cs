1da456d6cac53a1d9869e7ef44bf0822ed3ee10e
mrvnzcn <imervanozcan@gmail.com>
2024-02-05 00:13:49 +0300

    test2

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container. test
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
