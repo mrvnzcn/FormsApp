a8d4c7f4e8cd1f536569220f5beee7a61b66ae0a
mrvnzcn <imervanozcan@gmail.com>
2024-02-05 20:29:43 +0300

    Product List

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,10 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FormsApp.Models
 {
     public class Product
     {
+        [Display(Name="Ürün Id")]
         public Guid ProductId { get; set; }
+        [Display(Name="Ürün Adı")]
         public string? Name { get; set; }
+        [Display(Name="Fiyat")]
         public decimal Price { get; set; }
+        [Display(Name="Görsel")]
         public string? Image { get; set; }
         public bool isActive { get; set; }
         public Guid CategoryId { get; set; }
--- Models/Repository.cs
+++ Models/Repository.cs
@@ -7,7 +7,19 @@ namespace FormsApp.Models
 
         static Repository()
         {
-            
+            _categories.Add(new Category { CategoryId = Guid.NewGuid(), Name = "Telefon" });
+            _categories.Add(new Category { CategoryId = Guid.NewGuid(), Name = "Bilgisayar" });
+
+            Guid PhoneCategoryId = Guid.NewGuid();
+            Guid ComputerCategoryId = Guid.NewGuid();
+
+            _products.Add(new Product { ProductId = Guid.NewGuid(), Name="Iphone 14", Price = 40000, isActive = true, Image = "1.jpg", CategoryId = PhoneCategoryId});
+            _products.Add(new Product { ProductId = Guid.NewGuid(), Name="Iphone 15", Price = 50000, isActive = true, Image = "2.jpg", CategoryId = PhoneCategoryId});
+            _products.Add(new Product { ProductId = Guid.NewGuid(), Name="Iphone 16", Price = 60000, isActive = true, Image = "3.jpg", CategoryId = PhoneCategoryId});
+            _products.Add(new Product { ProductId = Guid.NewGuid(), Name="Iphone 17", Price = 70000, isActive = true, Image = "4.jpg", CategoryId = PhoneCategoryId});
+
+            _products.Add(new Product { ProductId = Guid.NewGuid(), Name="MacBook Air", Price = 80000, isActive = true, Image = "5.jpg", CategoryId = ComputerCategoryId});
+            _products.Add(new Product { ProductId = Guid.NewGuid(), Name="Macbook Pro", Price = 90000, isActive = true, Image = "6.jpg", CategoryId = ComputerCategoryId});
         }
 
         public static List<Product> Products
