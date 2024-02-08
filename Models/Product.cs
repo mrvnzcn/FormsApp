ffccc2a2b73086ce01da57c1c08c804a30ad0303
mrvnzcn <imervanozcan@gmail.com>
2024-02-09 00:20:03 +0300

    Form Validations

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,21 +7,26 @@ namespace FormsApp.Models
         [Display(Name="Ürün Id")]
         public int ProductId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Gerekli bir alan")]
+        [StringLength(100)]
         [Display(Name="Ürün Adı")]
         public string? Name { get; set; }
 
+        [Required]
+        [Range(0, 100000)]
         [Display(Name="Fiyat")]
-        public decimal Price { get; set; }
-
-        [Display(Name="Görsel")]
-        public string? Image { get; set; }
+        public decimal? Price { get; set; }
 
         [Required]
+        [Display(Name="Görsel")]
+        public string? Image { get; set; } = string.Empty;
+
+        
         public bool IsActive { get; set; }
 
         [Display(Name="Category")]
-        public int CategoryId { get; set; }
+        [Required]
+        public int? CategoryId { get; set; }
     }
 }
 
